8f7de9d6bb0541b8ab62e344c3e83ebea5324252
Gwénaël Després <gwenael.despres@gmail.com>
2021-02-06 11:58:36 +0100

    Replace javax.inject.Provider by Spring ObjectProvider and remove dependency

--- spring-boot-starter-httpclient-security-cas/src/main/java/io/github/springboot/httpclient/auth/cas/CasAuthenticator.java
+++ spring-boot-starter-httpclient-security-cas/src/main/java/io/github/springboot/httpclient/auth/cas/CasAuthenticator.java
@@ -1,10 +1,8 @@
 package io.github.springboot.httpclient.auth.cas;
 
-import java.net.URL;
-import java.util.List;
-
-import javax.inject.Provider;
-
+import io.github.springboot.httpclient.constants.HttpClientConstants;
+import io.github.springboot.httpclient.utils.HostUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpRequest;
 import org.apache.http.RequestLine;
@@ -15,106 +13,106 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.cookie.Cookie;
 import org.jasig.cas.client.util.AssertionHolder;
 import org.jasig.cas.client.validation.Assertion;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.cas.authentication.CasAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import io.github.springboot.httpclient.constants.HttpClientConstants;
-import io.github.springboot.httpclient.utils.HostUtils;
-import lombok.extern.slf4j.Slf4j;
+import java.net.URL;
+import java.util.List;
 
 @Slf4j
 public class CasAuthenticator {
 
-  @Autowired(required = false)
-  private Provider<CookieStore> cookieStore;
+    @Autowired
+    private ObjectProvider<CookieStore> cookieStoreProvider;
 
-  @Autowired
-  @Lazy
-  private Executor httpExecutor;
+    @Autowired
+    @Lazy
+    private Executor httpExecutor;
 
-  public void authCas(final HttpRequest request, String authenticationEndPoint) throws Exception {
-    Assertion assertion = AssertionHolder.getAssertion();
-    if (assertion == null) {
-      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      if (authentication != null) {
-        final CasAuthenticationToken token = (CasAuthenticationToken) authentication;
-        assertion = token.getAssertion();
-      }
-    }
-    if (assertion == null) {
-      throw new IllegalStateException("No cas assertion found for CAS proxy ticket authentification");
-    }
+    public void authCas(HttpRequest request, String authenticationEndPoint) throws Exception {
+        Assertion assertion = AssertionHolder.getAssertion();
+        if (assertion == null) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null) {
+                CasAuthenticationToken token = (CasAuthenticationToken) authentication;
+                assertion = token.getAssertion();
+            }
+        }
+        if (assertion == null) {
+            throw new IllegalStateException("No cas assertion found for CAS proxy ticket authentification");
+        }
 
-    String realAuthenticationEndPoint = authenticationEndPoint;
-    final RequestLine requestLine = request.getRequestLine();
-    final String requestUri = requestLine.getUri();
+        String realAuthenticationEndPoint = authenticationEndPoint;
+        RequestLine requestLine = request.getRequestLine();
+        String requestUri = requestLine.getUri();
 
-    final URL url = new URL(requestUri);
-    final String rootPath = HostUtils.getRootPath(url);
+        URL url = new URL(requestUri);
+        String rootPath = HostUtils.getRootPath(url);
 
-    if (StringUtils.isBlank(realAuthenticationEndPoint)) {
-      realAuthenticationEndPoint = HostUtils.getBaseUrl(url);
-      if (StringUtils.isNotBlank(rootPath)) {
-        realAuthenticationEndPoint += rootPath + HttpClientConstants.SLASH;
-      }
-      realAuthenticationEndPoint += HttpClientConstants.AUTH_LOGIN_PATH;
-    }
+        if (StringUtils.isBlank(realAuthenticationEndPoint)) {
+            realAuthenticationEndPoint = HostUtils.getBaseUrl(url);
+            if (StringUtils.isNotBlank(rootPath)) {
+                realAuthenticationEndPoint += rootPath + HttpClientConstants.SLASH;
+            }
+            realAuthenticationEndPoint += HttpClientConstants.AUTH_LOGIN_PATH;
+        }
 
-    if (StringUtils.startsWithIgnoreCase(requestUri, realAuthenticationEndPoint)) {
-      log.trace("Authentication request on URL ({}); no need to authenticate itself this request", request);
-    } else {
-      final String lastPath = StringUtils.substringAfterLast(url.getPath(), HttpClientConstants.SLASH);
-      if (StringUtils.startsWithIgnoreCase(lastPath, HttpClientConstants.PUBLIC_PATH)) {
-        log.debug("This URL ({}) is public, bypass CAS Authentication...", request);
-      } else {
-        log.debug("This URL ({}) is CAS protected, using Proxy Ticket mechanism to authenticate user", request);
-        final String sessionId = hasSessionOn(url, rootPath);
-
-        if (StringUtils.isBlank(sessionId)) {
-          log.debug("Using CAS Authentication for url : {} on endpoint {}", request, realAuthenticationEndPoint);
-          final String pt = assertion.getPrincipal().getProxyTicketFor(realAuthenticationEndPoint);
-          if (StringUtils.isNotBlank(pt)) {
-            log.debug("Using PT : {} for {}", pt, realAuthenticationEndPoint);
-            final URIBuilder builder = new URIBuilder(realAuthenticationEndPoint);
-            builder.addParameter(HttpClientConstants.TICKET_PARAMETER, pt);
-            final String authenticationUrl = builder.build().toString();
-            final String authenticationResponse = httpExecutor.execute(Request.Get(authenticationUrl)).returnContent()
-                .asString();
-            log.debug("PRE-Authenticating on URL : {}; gives : {}", authenticationUrl, authenticationResponse);
-          } else {
-            log.info("ProxyTicket for {} (CAS protected URL) is NULL, unable to authenticate through endpoint {}",
-                request, realAuthenticationEndPoint);
-          }
+        if (StringUtils.startsWithIgnoreCase(requestUri, realAuthenticationEndPoint)) {
+            log.trace("Authentication request on URL ({}); no need to authenticate itself this request", request);
         } else {
-          log.debug(
-              "Already authenticated on CAS protected URL ({}), no need to authenticate again, current sessionID : {}",
-              request, sessionId);
-        }
-      }
-    }
-  }
+            String lastPath = StringUtils.substringAfterLast(url.getPath(), HttpClientConstants.SLASH);
+            if (StringUtils.startsWithIgnoreCase(lastPath, HttpClientConstants.PUBLIC_PATH)) {
+                log.debug("This URL ({}) is public, bypass CAS Authentication...", request);
+            } else {
+                log.debug("This URL ({}) is CAS protected, using Proxy Ticket mechanism to authenticate user", request);
+                String sessionId = hasSessionOn(url, rootPath);
 
-  private String hasSessionOn(URL url, String rootPath) {
-    final CookieStore store = cookieStore.get();
-    if (store == null) {
-      return null;
+                if (StringUtils.isBlank(sessionId)) {
+                    log.debug("Using CAS Authentication for url : {} on endpoint {}", request, realAuthenticationEndPoint);
+                    String pt = assertion.getPrincipal().getProxyTicketFor(realAuthenticationEndPoint);
+                    if (StringUtils.isNotBlank(pt)) {
+                        log.debug("Using PT : {} for {}", pt, realAuthenticationEndPoint);
+                        URIBuilder builder = new URIBuilder(realAuthenticationEndPoint);
+                        builder.addParameter(HttpClientConstants.TICKET_PARAMETER, pt);
+                        String authenticationUrl = builder.build().toString();
+                        String authenticationResponse = httpExecutor.execute(Request.Get(authenticationUrl)).returnContent()
+                                .asString();
+                        log.debug("PRE-Authenticating on URL : {}; gives : {}", authenticationUrl, authenticationResponse);
+                    } else {
+                        log.info("ProxyTicket for {} (CAS protected URL) is NULL, unable to authenticate through endpoint {}",
+                                request, realAuthenticationEndPoint);
+                    }
+                } else {
+                    log.debug(
+                            "Already authenticated on CAS protected URL ({}), no need to authenticate again, current sessionID : {}",
+                            request, sessionId);
+                }
+            }
+        }
     }
 
-    String jSessionId = null;
-    final List<Cookie> cookies = store.getCookies();
-    if (cookies != null && !cookies.isEmpty()) {
-      for (final Cookie cookie : cookies) {
-        if (StringUtils.equalsIgnoreCase(cookie.getName(), HttpClientConstants.JESSION_ID_PARAMETER_NAME)
-            && StringUtils.equalsIgnoreCase(cookie.getDomain(), url.getHost())
-            && StringUtils.equalsIgnoreCase(cookie.getPath(), rootPath)) {
-          jSessionId = cookie.getValue();
-          break;
+    private String hasSessionOn(URL url, String rootPath) {
+        CookieStore store = cookieStoreProvider.getIfAvailable();
+        if (store == null) {
+            return null;
         }
-      }
+
+        String jSessionId = null;
+        List<Cookie> cookies = store.getCookies();
+        if (cookies != null && !cookies.isEmpty()) {
+            for (Cookie cookie : cookies) {
+                if (StringUtils.equalsIgnoreCase(cookie.getName(), HttpClientConstants.JESSION_ID_PARAMETER_NAME)
+                        && StringUtils.equalsIgnoreCase(cookie.getDomain(), url.getHost())
+                        && StringUtils.equalsIgnoreCase(cookie.getPath(), rootPath)) {
+                    jSessionId = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        return jSessionId;
     }
-    return jSessionId;
-  }
 }
--- spring-boot-starter-httpclient/src/main/java/io/github/springboot/httpclient/HttpClientProvider.java
+++ spring-boot-starter-httpclient/src/main/java/io/github/springboot/httpclient/HttpClientProvider.java
@@ -1,7 +1,15 @@
 package io.github.springboot.httpclient;
 
-import javax.inject.Provider;
-
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.httpclient.HttpClientMetricNameStrategies;
+import com.codahale.metrics.httpclient.HttpClientMetricNameStrategy;
+import com.codahale.metrics.httpclient.InstrumentedHttpRequestExecutor;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.springboot.httpclient.auth.CookieProcessingTargetAuthenticationStrategy;
+import io.github.springboot.httpclient.config.HttpClientConfigurationHelper;
+import io.github.springboot.httpclient.constants.ConfigurationConstants;
+import io.github.springboot.httpclient.internal.RequestConfigurer;
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.auth.AuthSchemeProvider;
@@ -24,109 +32,96 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.httpclient.HttpClientMetricNameStrategies;
-import com.codahale.metrics.httpclient.HttpClientMetricNameStrategy;
-import com.codahale.metrics.httpclient.InstrumentedHttpRequestExecutor;
-
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
-import io.github.springboot.httpclient.auth.CookieProcessingTargetAuthenticationStrategy;
-import io.github.springboot.httpclient.config.HttpClientConfigurationHelper;
-import io.github.springboot.httpclient.constants.ConfigurationConstants;
-import io.github.springboot.httpclient.internal.RequestConfigurer;
-
 /**
  * @author srouthiau
- *
  */
 @Configuration
 @ComponentScan("io.github.springboot.httpclient")
 public class HttpClientProvider {
 
-  @Autowired(required = false)
-  private Provider<CookieStore> cookieStore;
+    @Autowired
+    private ObjectProvider<CookieStore> cookieStoreProvider;
 
-  @Autowired
-  private ObjectProvider<HttpRequestInterceptor> requestInterceptors ;
+    @Autowired
+    private ObjectProvider<HttpRequestInterceptor> requestInterceptors;
 
-  @Autowired
-  private ObjectProvider<HttpResponseInterceptor> responseInterceptors ;
+    @Autowired
+    private ObjectProvider<HttpResponseInterceptor> responseInterceptors;
 
-  @Autowired
-  private RequestConfigurer configurer;
+    @Autowired
+    private RequestConfigurer configurer;
 
-  @Autowired
-  private HttpClientConfigurationHelper config;
+    @Autowired
+    private HttpClientConfigurationHelper config;
 
-  @Autowired
-  private Registry<AuthSchemeProvider> authSchemeRegistry;
+    @Autowired
+    private Registry<AuthSchemeProvider> authSchemeRegistry;
 
-  @Autowired
-  private RequestConfig defaultRequestConfig;
+    @Autowired
+    private RequestConfig defaultRequestConfig;
 
-  @Autowired
-  private CredentialsProvider credentialsProvider;
+    @Autowired
+    private CredentialsProvider credentialsProvider;
 
-  @Autowired
-  private HttpClientConnectionManager connectionManager;
+    @Autowired
+    private HttpClientConnectionManager connectionManager;
 
-  @Autowired
-  @Qualifier("legacyMetricRegistry")
-  private MetricRegistry metricRegistry;
+    @Autowired
+    @Qualifier("legacyMetricRegistry")
+    private MetricRegistry metricRegistry;
 
-  @Autowired
-  private CircuitBreakerRegistry cbRegistry;
+    @Autowired
+    private CircuitBreakerRegistry cbRegistry;
 
-  @Autowired
-  private RateLimiterRegistry rlRegistry;
+    @Autowired
+    private RateLimiterRegistry rlRegistry;
 
-  @Bean
-  public Executor httpClientExecutor(HttpClient httpClient) {
-    return Executor.newInstance(httpClient);
-  }
-
-  @Bean
-  // TODO CloseableHttpClientCustomizer for metrics InstrumentedHttpRequestExecutor
-  public CloseableHttpClient httpClient() {
-    // Create HttpClient
-    final HttpClientBuilder clientBuilder = HttpClientBuilder.create();
-    final HttpClientMetricNameStrategy metricNameStrategy = getMetricNameStrategy(
-        config.getGlobalConfiguration(ConfigurationConstants.METRIC_NANE_STRATEGY));
-    clientBuilder.setRequestExecutor(new InstrumentedHttpRequestExecutor(metricRegistry, metricNameStrategy));
-    clientBuilder.setConnectionManager(connectionManager);
-    clientBuilder.setDefaultAuthSchemeRegistry(authSchemeRegistry);
-    clientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-    clientBuilder.setDefaultRequestConfig(defaultRequestConfig);
-    clientBuilder.setTargetAuthenticationStrategy(CookieProcessingTargetAuthenticationStrategy.INSTANCE);
-
-    clientBuilder.setConnectionReuseStrategy(DefaultConnectionReuseStrategy.INSTANCE);
-    clientBuilder.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE);
-
-    final String userAgent = config.getGlobalConfiguration(ConfigurationConstants.USER_AGENT);
-    clientBuilder.setUserAgent(userAgent);
-
-    requestInterceptors.forEach(clientBuilder::addInterceptorLast);
-    responseInterceptors.forEach(clientBuilder::addInterceptorLast);
-
-    if (config.isCookieManagementDisabled() || cookieStore.get() == null) {
-      clientBuilder.disableCookieManagement();
-    } else {
-      clientBuilder.setDefaultCookieStore(cookieStore.get());
+    @Bean
+    public Executor httpClientExecutor(HttpClient httpClient) {
+        return Executor.newInstance(httpClient);
     }
 
-    return new ConfigurableHttpClient(config, cbRegistry, rlRegistry, clientBuilder.build(), configurer);
-  }
+    @Bean
+    // TODO CloseableHttpClientCustomizer for metrics InstrumentedHttpRequestExecutor
+    public CloseableHttpClient httpClient() {
+        // Create HttpClient
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        HttpClientMetricNameStrategy metricNameStrategy = getMetricNameStrategy(
+                config.getGlobalConfiguration(ConfigurationConstants.METRIC_NANE_STRATEGY));
+        clientBuilder.setRequestExecutor(new InstrumentedHttpRequestExecutor(metricRegistry, metricNameStrategy));
+        clientBuilder.setConnectionManager(connectionManager);
+        clientBuilder.setDefaultAuthSchemeRegistry(authSchemeRegistry);
+        clientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+        clientBuilder.setDefaultRequestConfig(defaultRequestConfig);
+        clientBuilder.setTargetAuthenticationStrategy(CookieProcessingTargetAuthenticationStrategy.INSTANCE);
 
-  private HttpClientMetricNameStrategy getMetricNameStrategy(String name) {
-    HttpClientMetricNameStrategy nameStrategy = HttpClientMetricNameStrategies.HOST_AND_METHOD;
-    if ("QUERYLESS_URL_AND_METHOD".equalsIgnoreCase(name)) {
-      nameStrategy = HttpClientMetricNameStrategies.QUERYLESS_URL_AND_METHOD;
-    } else if ("METHOD_ONLY".equalsIgnoreCase(name)) {
-      nameStrategy = HttpClientMetricNameStrategies.METHOD_ONLY;
+        clientBuilder.setConnectionReuseStrategy(DefaultConnectionReuseStrategy.INSTANCE);
+        clientBuilder.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE);
+
+        String userAgent = config.getGlobalConfiguration(ConfigurationConstants.USER_AGENT);
+        clientBuilder.setUserAgent(userAgent);
+
+        requestInterceptors.forEach(clientBuilder::addInterceptorLast);
+        responseInterceptors.forEach(clientBuilder::addInterceptorLast);
+
+        if (config.isCookieManagementDisabled() || cookieStoreProvider.getIfAvailable() == null) {
+            clientBuilder.disableCookieManagement();
+        } else {
+            clientBuilder.setDefaultCookieStore(cookieStoreProvider.getIfAvailable());
+        }
+
+        return new ConfigurableHttpClient(config, cbRegistry, rlRegistry, clientBuilder.build(), configurer);
     }
 
-    return nameStrategy;
-  }
+    private HttpClientMetricNameStrategy getMetricNameStrategy(String name) {
+        HttpClientMetricNameStrategy nameStrategy = HttpClientMetricNameStrategies.HOST_AND_METHOD;
+        if ("QUERYLESS_URL_AND_METHOD".equalsIgnoreCase(name)) {
+            nameStrategy = HttpClientMetricNameStrategies.QUERYLESS_URL_AND_METHOD;
+        } else if ("METHOD_ONLY".equalsIgnoreCase(name)) {
+            nameStrategy = HttpClientMetricNameStrategies.METHOD_ONLY;
+        }
+
+        return nameStrategy;
+    }
 
 }
