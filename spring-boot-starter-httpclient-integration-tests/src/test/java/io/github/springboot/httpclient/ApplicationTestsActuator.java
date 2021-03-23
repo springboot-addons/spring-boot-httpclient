099d521ac750a474b86b22100d9c7b94087203af
Stephane Routhiau <stephane.routhiau@gmail.com>
2021-03-23 09:45:39 +0100

    refactoring test + cleanup + integration tests

---
+++ spring-boot-starter-httpclient-integration-tests/src/test/java/io/github/springboot/httpclient/ApplicationTestsActuator.java
@@ -0,0 +1,71 @@
+package io.github.springboot.httpclient;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.util.EntityUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.github.springboot.httpclient.actuator.HttpClientEndpoint;
+
+/**
+ * http client auto configuration tests
+ *
+ * @author linux_china
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@ComponentScan("io.github.springboot.httpclient.core")
+public class ApplicationTestsActuator {
+
+	@Autowired
+	ApplicationContext context;
+
+	@Test
+	public void testHttpsClientWithStats() throws Exception {
+		final HttpClient httpClient = context.getBean(HttpClient.class);
+		final HttpGet httpGet = new HttpGet("https://httpbin.org/headers");
+		final HttpResponse response = httpClient.execute(httpGet);
+		EntityUtils.toString(response.getEntity());
+		HttpClientEndpoint stats = getStats();
+		Assert.assertNotNull(stats);
+		Assert.assertNotNull(stats.getMetrics());
+	}
+
+	@Test
+	public void testHttpClientPostSoTimeout() throws Exception {
+		final HttpClient httpClient = context.getBean(HttpClient.class);
+		final HttpPost httpPost = new HttpPost("https://httpbin.org/delay/4");
+		try {
+			httpClient.execute(httpPost);
+		} catch (final Exception e) {
+			Assert.fail("Timeout not should have occured");
+		}
+	}
+
+	@Test
+	public void testExecutor() throws Exception {
+		final Executor executor = context.getBean(Executor.class);
+		final String content = executor.execute(Request.Get("https://httpbin.org/headers")).returnContent().asString();
+		Assert.assertTrue(content.contains("httpclient"));
+	}
+
+	private HttpClientEndpoint getStats() {
+		final HttpClientEndpoint endpoint = context.getBean(HttpClientEndpoint.class);
+		System.out.println(ToStringBuilder.reflectionToString(endpoint, ToStringStyle.JSON_STYLE));
+		return endpoint;
+	}
+}
---
+++ spring-boot-starter-httpclient-integration-tests/src/test/java/io/github/springboot/httpclient/ApplicationTestsResilience4j.java
@@ -0,0 +1,115 @@
+package io.github.springboot.httpclient;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+
+/**
+ * http client auto configuration tests
+ *
+ * @author linux_china
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@ComponentScan("io.github.springboot.httpclient.core")
+public class ApplicationTestsResilience4j {
+
+	@Autowired
+	ApplicationContext context;
+
+	@Autowired
+	CircuitBreakerRegistry circuitBreakerRegistry;
+
+	@Test
+	public void testRateLimiter() throws Exception {
+		long begin = System.currentTimeMillis() ;
+		final HttpClient httpClient = context.getBean(HttpClient.class);
+		final HttpGet httpGet = new HttpGet("https://httpbin.org/headers");
+		for (int i = 0; i < 15; i++) {
+			final HttpResponse response = httpClient.execute(httpGet);
+			if (response != null && response.getEntity() != null) {
+				EntityUtils.toString(response.getEntity());
+			}
+		}
+		long end = System.currentTimeMillis() ;
+		Assert.assertTrue(end-begin > 10000);
+		
+	}
+
+	@Test
+	public void testHttpClientPostSoTimeout() throws Exception {
+		final HttpClient httpClient = context.getBean(HttpClient.class);
+		final HttpPost httpPost = new HttpPost("https://httpbin.org/delay/4");
+		try {
+			httpClient.execute(httpPost);
+		} catch (final Exception e) {
+			Assert.fail("Timeout not should have occured");
+		}
+	}
+
+	@Test
+	public void testExecutor() throws Exception {
+		final Executor executor = context.getBean(Executor.class);
+		final String content = executor.execute(Request.Get("https://httpbin.org/headers")).returnContent().asString();
+		Assert.assertTrue(content.contains("httpclient"));
+	}
+
+	@Test
+	public void testCircuitBreakerException() throws Exception {
+		final CloseableHttpClient httpClient = context.getBean(CloseableHttpClient.class);
+		final HttpGet httpGet = new HttpGet("https://www.unknown.com");
+		boolean hasRecovered = false;
+		boolean hasBeenBreaked = false;
+		for (int i = 0; i < 4; i++) {
+			try {
+				final HttpResponse response = httpClient.execute(httpGet);
+				System.out.println(response.getStatusLine());
+				Assert.assertTrue(response.getStatusLine().getReasonPhrase().contains("Broken circuit"));
+				Assert.assertTrue(response.getStatusLine().getStatusCode() == 503);
+				hasBeenBreaked = true;
+				Thread.sleep(3100);
+			} catch (final Exception e) {
+				if (e instanceof IOException && e.getMessage().contains("Broken circuit")) {
+					hasBeenBreaked = true;
+				}
+				hasRecovered = hasBeenBreaked;
+				System.out.println("Error : " + e.getMessage());
+			}
+		}
+		Assert.assertTrue(hasBeenBreaked);
+		Assert.assertTrue(hasRecovered);
+	}
+
+	@Test
+	public void testCircuitBreakerHttp503() throws Exception {
+		final HttpClient httpClient = context.getBean(HttpClient.class);
+		final HttpGet httpGet = new HttpGet("https://httpbin.org/status/503");
+		for (int i = 0; i < 5; i++) {
+			final HttpResponse response = httpClient.execute(httpGet);
+			Assert.assertTrue(response.getStatusLine().getStatusCode() == 503);
+		}
+		final HttpResponse response = httpClient.execute(httpGet);
+		System.out.println(response.getStatusLine());
+		Assert.assertTrue(response.getStatusLine().getReasonPhrase().contains("Broken circuit"));
+		Assert.assertTrue(response.getStatusLine().getStatusCode() == 503);
+		circuitBreakerRegistry.find("httpbin-org").get().reset();
+	}
+}
