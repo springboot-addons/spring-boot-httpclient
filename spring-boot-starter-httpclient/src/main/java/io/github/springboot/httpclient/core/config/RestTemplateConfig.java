06318d7c8e1c758a7fa9bf421f55886949ed8f59
srouthiau <stephane.routhiau@capsi-informatique.fr>
2021-03-29 10:38:46 +0200

    add resttemplate config

---
+++ spring-boot-starter-httpclient/src/main/java/io/github/springboot/httpclient/core/config/RestTemplateConfig.java
@@ -0,0 +1,28 @@
+package io.github.springboot.httpclient.core.config;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ConditionalOnProperty(name = "httpclient.core.autoconfigure-rest-template", havingValue = "true", matchIfMissing = true)
+public class RestTemplateConfig {
+
+    @Autowired
+    CloseableHttpClient httpClient;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setHttpClient(httpClient);
+        return clientHttpRequestFactory;
+    }
+}
